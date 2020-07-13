8e4bfaef17b2e8307e6022faf554befb946b36da
lvv5040 <66978068+lvv5040@users.noreply.github.com>
2020-07-12 20:24:23 -0600

    its functioning but I am not a fan of the colors and size

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var confirmSymbol;
 var confirmUppercase;
 var confirmLowercase;
 
-// the characters that are possible
+// Possible characters
 symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-number = [1, 2, 3, 4, 5, 6, 7, 8 ,9];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 character = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-space = [],
+space = [];
 
 var choices;
 
@@ -18,7 +18,7 @@ var toUpper = function (x) {
 
 lowC = character.map(toUpper);
 
-var get = document.querySelector(#generate);
+var get = document.querySelector("#generate");
 
 get.addEventListener("click", function () {
     ps = generatePassword();
@@ -28,102 +28,90 @@ get.addEventListener("click", function () {
 function generatePassword() {
     enter = parseInt(prompt("Must be between 7 and 70 characters"));
     if (!enter) {
-        alert("This needs a value added");
-    } else if (enter < 7 || enter > 70) {
-        enter = parseInt(prompt("Chose between 7 and 70 characters"));
+        alert("There needs to be a value");
+    } else if (enter < 8 || enter > 128) {
+        enter = parseInt(prompt("Choose between 7 and 70 characters!"));
+
     } else {
-        confirmNumber = confirm("Do you want any numbers in your password?");
-        confirmSymbol = confirm("Do you want any symbols in your password?");
-        confirmUppercase = confirm("Do you want any uppercase letters in your password?");
-        confirmLowercase = confirm("Do you want any lowercase letters in your password?");
+        confirmNumber = confirm("Would you like any numbers in your password?");
+        confirmSymbol = confirm("Would you like any symbols in your password?");
+        confirmUppercase = confirm("Would you like uppercase letters in your password?");
+        confirmLowercase = confirm("Would you like lowercase letters in your password?");
     };
 
-    if (!confirmNumber && !confirmSymbol && !confirmUppercase && !confirmLowercase) {
-        choices = alert("You must choose at least one!");
-    }
+    
+    if (!confirmSymbol && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        choices = alert("You must pick at least one!");
 
-    //all of these are positive
-    else if (confirmNumber && confirmSymbol && confirmUppercase &&confirmLowercase) {
+    }
+//all positve
+    else if (confirmSymbol && confirmNumber && confirmUppercase && confirmLowercase) {
+
         choices = symbol.concat(number, character, lowC);
     }
-
-    //three positive
-    else if (confirmNumber && confirmSymbol && confirmUppercase) {
-    choices = symbol.concat(number, lowC);
+//3 positive    
+    else if (confirmSymbol && confirmNumber && confirmUppercase) {
+        choices = symbol.concat(number, lowC);
     }
-
-    else if (confirmNumber && confirmSymbol && confirmLowercase) {
+    else if (confirmSymbol && confirmNumber && confirmLowercase) {
         choices = symbol.concat(number, character);
     }
-
-    else if (confirmSymbol && confirmUppercase && confirmLowercase) {
+    else if (confirmSymbol && confirmLowercase && confirmUppercase) {
         choices = symbol.concat(character, lowC);
     }
-
-    else if (confirmNumber && confirmUppercase && confirmLowercase) {
-        choices = symbol.concat(character, lowC)
+    else if (confirmNumber && confirmLowercase && confirmUppercase) {
+        choices = number.concat(character, lowC);
     }
-
-    //two positive
-    else if (confirmNumber && confirmSymbol) {
+//2 positive 
+    else if (confirmSymbol && confirmNumber) {
         choices = symbol.concat(number);
-    }
 
-    else if (confirmSymbol && confirmLowercase) {
+    } else if (confirmSymbol && confirmLowercase) {
         choices = symbol.concat(character);
-    }
 
-    else if (confirmSymbol && confirmUppercase) {
+    } else if (confirmSymbol && confirmUppercase) {
         choices = symbol.concat(lowC);
     }
+    else if (confirmLowercase && confirmNumber) {
+        choices = character.concat(number);
 
-    else if (confirmNumber && confirmLowercase) {
-        choices = symbol.concat(number);
+    } else if (confirmLowercase && confirmUppercase) {
+        choices = character.concat(lowC);
+
+    } else if (confirmNumber && confirmUppercase) {
+        choices = number.concat(lowC);
     }
-
-    else if (confirmUppercase && confirmLowercase) {
-        choices = symbol.conact(lowC);
-    }
-
-    else if (confirmNumber && confirmUppercase) {
-        choices = symbol.concat(lowC);
-    }
-
-    //one postive
-    else if (confirmNumber) {
-        choices = number;
-    }
-
-
+    //1 positive
     else if (confirmSymbol) {
         choices = symbol;
     }
-
-    else if (confirmUppercase) {
-        choices = space.concat(lowC);
+    else if (confirmNumber) {
+        choices = number;
     }
-
     else if (confirmLowercase) {
         choices = character;
     }
+    
+    else if (confirmUppercase) {
+        choices = space.concat(lowC);
+    };
 
-    var password [];
+    
+    var password = [];
 
-    for (car i = 0; i < enter; i++) {
+    
+    for (var i = 0; i < enter; i++) {
         var pickChoices = choices[Math.floor(Math.random() * choices.length)];
         password.push(pickChoices);
     }
-
+//I had to ask the internet about this one    
     var ps = password.join("");
     UserInput(ps);
     return ps;
 }
-
-//print 
-
+//prints to box
 function UserInput(ps) {
     document.getElementById("password").textContent = ps;
+
 }
 
-
-  
