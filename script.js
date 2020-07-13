9091fd357d9fd46b5283943a62aaae49e59e9739
lvv5040 <66978068+lvv5040@users.noreply.github.com>
2020-07-12 18:51:31 -0600

    adding the the javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+var enter;
+var confirmNumber;
+var confirmSymbol;
+var confirmUppercase;
+var confirmLowercase;
+
+// the characters that are possible
+symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+number = [1, 2, 3, 4, 5, 6, 7, 8 ,9];
+character = ["a", "b", "c", "d", "e", "f", ] 
